63d86b60ec1efc50e240aaa57afb08c2a3d629c9
Yangyu Chen <cyy@cyyself.name>
2020-11-23 15:13:48 +0800

    LAB2 finished

--- kern/mm/default_pmm.c
+++ kern/mm/default_pmm.c
@@ -109,20 +109,18 @@ default_init(void) {
 // 这里需要大家去理解通用链表结构 即list_entry_t与Page结构的关系
 // 注意在分配和回收Page时候，注意设置FLAGS property等数据结构
 // 也鼓励尝试Best-fit Worst-fit算法的设计与实现
-// TODO: 重写函数
 static void
 default_init_memmap(struct Page *base, size_t n) {
     assert(n > 0);
-    struct Page *p = base;
-    for (; p != base + n; p ++) {
-        assert(PageReserved(p));
-        p->flags = p->property = 0;
-        set_page_ref(p, 0);
+    {//scan page status, only for debug
+        struct Page *p = base;
+        for (; p != base + n; p ++) assert(PageReserved(p));
     }
+    memset(base,0,sizeof(struct Page)*n);//memset did fast
     base->property = n;
     SetPageProperty(base);
     nr_free += n;
-    list_add_before(&free_list, &(base->page_link));
+    list_add_before(&free_list,&(base->page_link));
 }
 
 static struct Page *
@@ -131,14 +129,13 @@ default_alloc_pages(size_t n) {
     if (n > nr_free) {
         return NULL;
     }
+    //best-fit algorithm
     struct Page *page = NULL;
     list_entry_t *le = &free_list;
-    // TODO: optimize (next-fit)
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
         if (p->property >= n) {
-            page = p;
-            break;
+            if (page == NULL || p->property < page->property) page = p;
         }
     }
     if (page != NULL) {
@@ -158,42 +155,36 @@ default_alloc_pages(size_t n) {
 static void
 default_free_pages(struct Page *base, size_t n) {
     assert(n > 0);
-    struct Page *p = base;
-    for (; p != base + n; p ++) {
-        assert(!PageReserved(p) && !PageProperty(p));
-        p->flags = 0;
-        set_page_ref(p, 0);
+    {//scan page status, only for debug
+        struct Page *p = base;
+        for (; p != base + n; p ++) assert(!PageReserved(p) && !PageProperty(p));
     }
+    memset(base,0,sizeof(struct Page)*n);//memset did fast
     base->property = n;
     SetPageProperty(base);
     list_entry_t *le = list_next(&free_list);
+    list_entry_t *insert_before = &free_list;
     while (le != &free_list) {
-        p = le2page(le, page_link);
+        struct Page *p = le2page(le, page_link);
         le = list_next(le);
-        // TODO: optimize
-        if (base + base->property == p) {
-            base->property += p->property;
-            ClearPageProperty(p);
-            list_del(&(p->page_link));
-        }
-        else if (p + p->property == base) {
+        if (p + p->property == base) {
             p->property += base->property;
             ClearPageProperty(base);
             base = p;
             list_del(&(p->page_link));
         }
-    }
-    nr_free += n;
-    le = list_next(&free_list);
-    while (le != &free_list) {
-        p = le2page(le, page_link);
-        if (base + base->property <= p) {
-            assert(base + base->property != p);
+        else if (base + base->property == p) {
+            base->property += p->property;
+            ClearPageProperty(p);
+            list_del(&(p->page_link));
+        }
+        else if (p > base) {
+            insert_before = &p->page_link;
             break;
         }
-        le = list_next(le);
     }
-    list_add_before(le, &(base->page_link));
+    nr_free += n;
+    list_add_before(insert_before, &(base->page_link));
 }
 
 static size_t
