443718f6ca2b93c3a62392627609abfe8dac14f9
Yangyu Chen <cyy@cyyself.name>
2020-12-18 11:21:03 +0800

    clear useless comment

--- kern/mm/pmm.c
+++ kern/mm/pmm.c
@@ -413,10 +413,9 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
             uintptr_t pa = page2pa(page_table);// (5) get linear address of page
             //page2pa returns shifted 
             memset(KADDR(pa),0,PGSIZE);// (6) clear page content using memset
-            *pdep = pa | PTE_P | PTE_W | PTE_U;//I don't think it's user can access but the answer from THU operate bitwise or PTE_U
+            *pdep = pa | PTE_P | PTE_W | PTE_U;
         }
         else return NULL;//if didn't will get panic
-        //TODO: 但是页表不存在又不创建，为什么不能panic？检查defalt_pmm.c中的函数逻辑
     }
     return KADDR(PDE_ADDR(*pdep)+(PTX(la) << 2));// (8) return page table entry
 }
